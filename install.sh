2c7f93968f0f6c210fe29b90d1f016a5ec1d0538
pkch93 <dev.pkch@gmail.com>
2023-05-06 14:05:26 +0900

    feature: install.sh 추가

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -u
+
+REPOSITORY_URL="https://github.com/pkch93/spring-boot-boilerplate"
+
+abort() {
+    printf "%s\n" "$@" >&2
+    exit 1
+}
+
+installBoilerPlate() {
+    mkdir boilerplate
+    cd boilerplate
+    git init
+    git config core.sparseCheckout true
+    git remote add -f origin $REPOSITORY_URL
+    echo "$0/*" > .git/info/sparse-checkout
+    git pull origin main
+    git checkout main
+    rm -rf .git
+
+    echo "finish spring-boot-boilerplate download!"
+}
+
+if [ -z "${BASH_VERSION:-}" ]
+then
+    abort "Bash is required to interpret this script."
+fi
+if [ -z `command -v git --version 2>/dev/null` ]
+then
+    abort "Git is required to install this boilerplates"
+fi
+
+boilerplates="spring-boot-react-kts spring-boot-kts"
+
+select boilerplate in $boilerplates ; do
+    if [ "$boilerplate" = "spring-boot-react-kts" ] ; then
+        installBoilerPlate "spring-boot-react-kts"
+        exit
+    elif [ "$boilerplate" = "spring-boot-kts" ] ; then
+        installBoilerPlate "spring-boot-kts"
+        exit
+    else
+        abort "not supported"
+    fi
+done
