7044f58f4bfe44843b91f967cdd76ad64487b915
pkch93 <dev.pkch@gmail.com>
2023-05-06 10:21:51 +0900

    [spring-boot-kotlin] Gradle 및 라이브러리 버전 수정

--- spring-boot-kts/buildSrc/src/main/kotlin/Versions.kt
+++ spring-boot-kts/buildSrc/src/main/kotlin/Versions.kt
@@ -1,21 +1,21 @@
 object Versions {
     // java & kotlin
     const val JVM = "17"
-    const val KOTLIN = "1.8.10"
+    const val KOTLIN = "1.8.21"
     const val KOTLIN_LOGGER = "3.0.5"
     const val DETEKT = "1.22.0"
 
     // spring
-    const val SPRING_BOOT = "3.0.4"
+    const val SPRING_BOOT = "3.0.6"
     const val SPRING_DEPENDENCY_MANAGEMENT = "1.1.0"
 
     // data
-    const val INFOBIP = "8.1.1"
+    const val INFOBIP = "8.1.2"
     const val QUERYDSL = "5.0.0"
 
     // mapper
-    val MAPSTRUCT = "1.5.3.Final"
+    val MAPSTRUCT = "1.5.5.Final"
 
     // testContainers
-    const val TEST_CONTAINERS = "1.17.6"
+    const val TEST_CONTAINERS = "1.18.0"
 }
